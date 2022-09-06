ec6e00f862f6eb05e350392ff1daf9011d6be58d
phamjmanh <56101064+phamjmanh@users.noreply.github.com>
2022-09-06 23:08:35 +0700

    code new

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ProjectNew/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ProjectNew from './components/ProjectNew.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ProjectNew
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
